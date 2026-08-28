3894f1deb5477f3bb0a8f1b666bcdc1302dab44b
barriazaa <barriazaa@miumg.edu.gt>
2026-08-28 07:55:53 -0600

    Se termino el paginador

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 let paso = 1;
+const pasoInicioal = 1;
+const pasoFinal =3;
 
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
@@ -7,6 +9,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function iniciarApp() {
     mostrarSeccion(); //Muestra y oculta las secciones
     tabs(); // Cambia la sección cuando se presionen los tabs
+    botonesPaginador(); //Agrega o quita los botones de paginador
+    paginaSiguiente();
+    paginaAnterior();
 }
 
 function mostrarSeccion() {
@@ -41,8 +46,45 @@ function tabs() {
             paso = parseInt(e.target.dataset.paso);
             
             mostrarSeccion();
+
+            botonesPaginador();
         });
     })
 } 
+
+function botonesPaginador() {
+    const paginaAnterior = document.querySelector('#anterior');
+    const paginaSiguiente = document.querySelector('#siguiente');
+
+    if(paso === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (paso === 3) {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.add('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', function() {
+        if(paso <= pasoInicioal) return;
+        paso--;
+
+        botonesPaginador();
+    })
+}
+function paginaSiguiente() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', function() {
+        if(paso >= pasoInicioal) return;
+        paso++ ;
+
+        botonesPaginador();
+    })
+}
 
       
